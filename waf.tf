879a4d953af3d4637da3b6aad388462d8a2456f2
Sankalp Gunturi <sgunturi@cmu.edu>
2023-11-28 15:40:11 -0800

    Update comments on WAF

--- waf.tf
+++ waf.tf
@@ -4,6 +4,8 @@ module "cloud_armor" {
   name           = "web-application-security-policy"
   description    = "Basic Cloud Armor security policy for DoS protection"
   default_rule_action = "deny"
+
+  # Set the security policy to use the default action
   layer_7_ddos_defense_enable          = true
   layer_7_ddos_defense_rule_visibility = "STANDARD"
   log_level                            = "VERBOSE"
@@ -14,7 +16,7 @@ module "cloud_armor" {
       action    = "deny(403)"
       priority  = 1
       description = "Deny all requests for DoS protection"
-      src_ip_ranges = ["0.0.0.0/0"]  # Dummy IP range, since we are using default_action = "deny"
+      src_ip_ranges = ["0.0.0.0/0"]  # [Assumption] Dummy IP range, since we are using default_action = "deny"
     }
   }
 }
