a583df15a5841fc6b435f0108a603f6a92c01545
Sankalp Gunturi <sgunturi@cmu.edu>
2023-11-27 23:14:56 -0800

    Update API response message

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', async function(req, res) {
-	res.send('Hello I am an API response!');
+	res.send('Hello I am an API response! - new');
 });
 
 app.listen(process.env.PORT || 80);
